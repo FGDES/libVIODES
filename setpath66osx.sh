577f4024263e468a65a485dc296cbc4d098f04dc
QElis <thomas.moor@fau.de>
2024-02-18 10:29:42 +0100

    debug

--- setpath66osx.sh
+++ setpath66osx.sh
@@ -1,6 +1,7 @@
 echo ==================== "select qt release"
 
 export PATH=~/Qt/6.6.2/macos/bin:$PATH
+export PATH=~/Qt/6.6.2/macos/libexec:$PATH
 echo using "$(which qmake)"
 
 echo ==================== set version in qmake database
