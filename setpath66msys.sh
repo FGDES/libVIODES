69f70998621e884b8074c5e184743e686518a316
Thomas Moor <thomas.moor@fau.de>
2024-03-09 18:29:08 +0100

    msys

--- setpath66msys.sh
+++ setpath66msys.sh
@@ -1,6 +1,6 @@
 echo ==================== "select qt release"
 
-export PATH=/c/Qt/6.6.2/mingw-64/bin:$PATH
+export PATH=/c/Qt/6.6.2/mingw_64/bin:$PATH
 echo using "$(which qmake)"
 
 echo ==================== set version in qmake database
