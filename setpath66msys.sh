cfaaf6880b2d6c5c99bf8bf58913e789096b2846
Thomas Moor <thomas.moor@fau.de>
2024-03-09 18:27:42 +0100

    msys

---
+++ setpath66msys.sh
@@ -0,0 +1,13 @@
+echo ==================== "select qt release"
+
+export PATH=/c/Qt/6.6.2/mingw-64/bin:$PATH
+echo using "$(which qmake)"
+
+echo ==================== set version in qmake database
+
+. ./VERSION
+VIODES_VERSION=$VIODES_VERSION_MAJOR.$VIODES_VERSION_MINOR
+qmake -set VIODES_VERSION $VIODES_VERSION 
+qmake -set VIODES_VERSION_MAJOR $VIODES_VERSION_MAJOR 
+qmake -set VIODES_VERSION_MINOR $VIODES_VERSION_MINOR 
+
