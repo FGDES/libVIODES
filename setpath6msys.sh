f0601c7459bafaa3dddc675889a3fc626a21e8ae
QElis <thomas.moor@fau.de>
2024-06-24 16:39:58 +0200

    mingw

--- setpath6msys.sh
+++ setpath6msys.sh
@@ -5,11 +5,4 @@ echo using MinGW "$(which g++)"
 export PATH=/C/Qt/6.6.2/mingw_64/bin:$PATH
 echo using Qt "$(which qmake)"
 
-echo ==================== set version in qmake database
-
-. ./VERSION
-PLANTSIM_VERSION=$PLANTSIM_VERSION_MAJOR.$PLANTSIM_VERSION_MINOR
-qmake -set PLANTSIM_VERSION $PLANTSIM_VERSION 
-qmake -set PLANTSIM_VERSION_MAJOR $PLANTSIM_VERSION_MAJOR 
-qmake -set PLANTSIM_VERSION_MINOR $PLANTSIM_VERSION_MINOR 
 
