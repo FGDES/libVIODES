a4ada9be39f23732fe3f4436095b722bf785870d
QElis <thomas.moor@fau.de>
2025-05-09 01:16:22 +0200

    msys

--- setpath6msys.sh
+++ setpath6msys.sh
@@ -1,8 +1,8 @@
 echo ==================== "select qt release and matching toolchain"
 
-export PATH=/C/Qt/Tools/mingw1120_64/bin:$PATH
+export PATH=/C/Qt/Tools/mingw1310_64/bin:$PATH
 echo using MinGW "$(which g++)"
-export PATH=/C/Qt/6.6.2/mingw_64/bin:$PATH
+export PATH=/C/Qt/6.8.3/mingw_64/bin:$PATH
 echo using Qt "$(which qmake)"
 
 
