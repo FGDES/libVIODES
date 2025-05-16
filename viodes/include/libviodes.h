296b62663f1101247518d977be26dfbf00d20a66
QElis <thomas.moor@fau.de>
2025-05-16 19:40:21 +0200

    xcode

---
+++ viodes/include/libviodes.h
@@ -0,0 +1,30 @@
+/* libviodes.h  - top level include for viodes library  */
+
+
+/*
+   Graphical IO for FAU Discrete Event Systems Library (libfaudes)
+
+   Copyright (C) 2006, 2007  Thomas Moor, Klaus Schmidt, Sebastian Perk
+   Copyright (C) 2010-2024 Thomas Moor
+
+*/
+
+
+
+#ifndef FAUDES_LIBVIODES_H
+#define FAUDES_LIBVIODES_H
+
+#include "libfaudes.h"
+#include "viostyle.h"
+#include "vioelement.h"
+#include "viosymbol.h"
+#include "viotoken.h"
+#include "vioconsole.h"
+#include "viotypes.h"
+#include "vioregistry.h"
+#include "vioattrstyle.h"
+#include "vioattribute.h"
+#include "vionsetstyle.h"
+#include "vionameset.h"
+
+#endif
